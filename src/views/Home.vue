8b772b4274908902bc43448d595026c5d74d3859
腾达 吴 <wtengda0816@outlook.com>
2019-05-30 09:41:54 +0800

    modify home spectras to spectra

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,6 @@
 <template>
   <div>
-    <v-alert :value="alertShow" type="warning">
-      {{ alertMessage }}
-    </v-alert>
+    <v-alert :value="alertShow" type="warning">{{ alertMessage }}</v-alert>
 
     <v-container>
       <v-layout wrap>
@@ -10,14 +8,13 @@
           <h2>导入</h2>
         </v-flex>
         <v-flex xs12 text-xs-left>
-          <v-btn color="primary" @click="importSpectraFromFile()">
-            <v-icon left dark>folder_open</v-icon>
-            从本地打开
+          <v-btn color="primary" @click="importSpectrumFromFile()">
+            <v-icon left dark>folder_open</v-icon>从本地打开
           </v-btn>
         </v-flex>
       </v-layout>
     </v-container>
-    
+
     <v-container fluid grid-list-xl>
       <v-layout row wrap>
         <v-flex xs12 pb-2>
@@ -25,45 +22,39 @@
         </v-flex>
 
         <v-flex text-xs-center v-if="onLoading">
-          <v-progress-circular
-            indeterminate
-            color="primary">
-          </v-progress-circular>
+          <v-progress-circular indeterminate color="primary"></v-progress-circular>
         </v-flex>
 
-        <v-flex
-          v-for="i in numRecentSpectras"
-          :key="i"
-          xs12 sm6 md4 lg4 xl4>
+        <v-flex v-for="i in numRecentSpectra" :key="i" xs12 sm6 md4 lg4 xl4>
           <v-hover>
-            <v-card 
-              slot-scope="{ hover }" 
-              :class="`elevation-${hover ? 12 : 2}`" 
-              @click="openSpectra(i - 1)" 
-              to="/preprocess">
+            <v-card
+              slot-scope="{ hover }"
+              :class="`elevation-${hover ? 12 : 2}`"
+              @click="openSpectrum(i - 1)"
+              to="/preprocess"
+            >
               <v-responsive :aspect-ratio="4/3">
-                <spectrum :datas="[spectras[i - 1]]"></spectrum>
+                <spectrum :datas="[spectra[i - 1]]"></spectrum>
               </v-responsive>
             </v-card>
           </v-hover>
         </v-flex>
       </v-layout>
     </v-container>
-
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import Component from 'vue-class-component';
-import Spectrum from '@/components/Spectrum.vue';
-import {remote} from 'electron';
-import fs from 'fs';
-import {Series} from '@/utils';
-import { AxiosResponse } from 'axios';
-import store from '../store'
-import path from 'path';
-import SpectraRepository from '../repositories/SpectraRepository'
+import Vue from "vue";
+import Component from "vue-class-component";
+import Spectrum from "@/components/Spectrum.vue";
+import { remote } from "electron";
+import fs from "fs";
+import { Series } from "@/utils";
+import { AxiosResponse } from "axios";
+import store from "../store";
+import path from "path";
+import SpectraRepository from "../repositories/SpectraRepository";
 
 @Component({
   components: {
@@ -72,9 +63,9 @@ import SpectraRepository from '../repositories/SpectraRepository'
 })
 export default class Home extends Vue {
   onLoading: boolean = true;
-  numRecentSpectras: number = 0;
-  spectras: Array<Series> = [];
-  alertMessage: string = '';
+  numRecentSpectra: number = 0;
+  spectra: Array<Series> = [];
+  alertMessage: string = "";
   alertShow: boolean = false;
 
   constructor() {
@@ -86,8 +77,8 @@ export default class Home extends Vue {
     SpectraRepository.loadSpectra()
       .then((response: AxiosResponse) => {
         for (const s of response.data.spectra) {
-          this.spectras.push({name: s.name, data: s.data});
-          this.numRecentSpectras = this.spectras.length;
+          this.spectra.push({ name: s.name, data: s.data });
+          this.numRecentSpectra = this.spectra.length;
         }
       })
       .catch((error: any) => {
@@ -99,42 +90,55 @@ export default class Home extends Vue {
       });
   }
 
-  openSpectra(i: number) {
-    this.postData(this.spectras[i]);
+  openSpectrum(i: number) {
+    this.postData(this.spectra[i]);
   }
 
-  importSpectraFromFile() {
-    const selectedFilePaths = remote.dialog.showOpenDialog({properties: ['openFile']});
-    if (selectedFilePaths === undefined) { // skip if not select
+  importSpectrumFromFile() {
+    const selectedFilePaths = remote.dialog.showOpenDialog({
+      properties: ["openFile"]
+    });
+    if (selectedFilePaths === undefined) {
+      // skip if not select
       return;
-    } 
-    else {
+    } else {
       // if select many, we only choose the first one
       fs.readFile(selectedFilePaths[0], (err, data) => {
         if (err) {
           return console.error(err);
-        } 
-        else {
+        } else {
           // get filename as spectra's name
           let name = path.parse(selectedFilePaths[0]).name;
           // access points
           let points = new Array<Array<number>>();
-          data.toString().trim().split('\n').map(line => {
-            let s = line.split('\t');
-            points.push([parseFloat(s[0]), parseFloat(s[1])]);
-          });
-          // data should be posted before preprocess page created, since 
+          data
+            .toString()
+            .trim()
+            .split("\n")
+            .map(line => {
+              let s = line.split("\t");
+              points.push([parseFloat(s[0]), parseFloat(s[1])]);
+            });
+          SpectraRepository.addSpectrum(name, points)
+            .then((response: AxiosResponse) => {
+              // get id
+            })
+            .catch((error: any) => {
+              this.alertMessage = `${error}`;
+              this.alertShow = true;
+            });
+          // data should be posted before preprocess page created, since
           // this page need data to construct.
-          this.postData({name: name, data: points});
-          this.$router.push('/preprocess');
+          this.postData({ name: name, data: points });
+          this.$router.push("/preprocess");
         }
-      })
+      });
     }
   }
 
   private postData(data: Series) {
-    store.commit('clear');
-    store.commit('enqueue', data);
+    store.commit("clear");
+    store.commit("enqueue", data);
   }
 }
 </script>
