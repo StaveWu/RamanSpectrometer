f24f64f6f823d83015f3e82784e1c695ba18a5bb
腾达 吴 <wtengda0816@outlook.com>
2019-06-11 15:04:22 +0800

    use domain deployed

--- src/repositories/Repository.ts
+++ src/repositories/Repository.ts
@@ -1,6 +1,7 @@
 import Axios, { AxiosResponse } from 'axios';
 
-const baseDomain = 'http://127.0.0.1:5000';
+// const baseDomain = 'http://127.0.0.1:5000';
+const baseDomain = 'https://raman-spectrometer.herokuapp.com';
 const baseURL = `${baseDomain}/api/v1`;
 
 export default Axios.create({
